bb803a70af11af5ccce0f15d57a33f57ff493eb7
ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
2023-02-11 15:03:47 +0200

    Add Unlicense header everywhere

--- app/src/main/java/io/github/leo40git/sltbg/app/resources/GamePalette.java
+++ app/src/main/java/io/github/leo40git/sltbg/app/resources/GamePalette.java
@@ -1,3 +1,12 @@
+/*
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain worldwide.
+ * This software is distributed without any warranty.
+ *
+ * A copy of the Unlicense should have been supplied as LICENSE in this repository.
+ * Alternatively, you can find it at <https://unlicense.org/>.
+ */
+
 package io.github.leo40git.sltbg.app.resources;
 
 import java.awt.Color;
