c8e1d3ad142d04748121d98093c03fbc595fcdfc
ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
2023-03-07 18:29:38 +0200

    oops, licenses

--- shared/src/main/java/io/leo40git/sltbg/gamedata/io/FaceImageWriteObserver.java
+++ shared/src/main/java/io/leo40git/sltbg/gamedata/io/FaceImageWriteObserver.java
@@ -1,3 +1,12 @@
+/*
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain worldwide.
+ * This software is distributed without any warranty.
+ *
+ * A copy of the Unlicense should have been supplied as LICENSE in this repository.
+ * Alternatively, you can find it at <https://unlicense.org/>.
+ */
+
 package io.leo40git.sltbg.gamedata.io;
 
 import io.leo40git.sltbg.gamedata.Face;
