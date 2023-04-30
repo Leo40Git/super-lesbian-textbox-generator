efefc0efb86e7e4d3c994c35e65b070391744c3d
ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
2023-04-30 21:30:28 +0300

    app: TextParserTest: add CSS-style 3 digit color test, use text block for source

--- app/src/test/java/io/github/leo40git/sltbg/app/test/TextParserTest.java
+++ app/src/test/java/io/github/leo40git/sltbg/app/test/TextParserTest.java
@@ -23,7 +23,12 @@ public final class TextParserTest {
         GameAssets.setPaletteColor(0, Color.WHITE);
         GameAssets.setPaletteColor(14, new Color(255, 241, 120));
 
-        String source = "\\c[14]Melody\n\\c[0]Bunny stew is \\\n\\++++\\sb\\c[#BB2929]delicious!\\sb\n\\si\\;\\si;";
+        String source = """
+                \\c[14]Melody
+                \\c[0]\\c[#FFF]\\c[#FFFFFF]\\
+                Bunny stew is\s
+                \\++++\\sb\\c[#BB2929]delicious!\\sb
+                \\si\\;\\si;""";
 
         System.out.println(" === preserveInvisible is false === ");
         dumpElements(source, false);
